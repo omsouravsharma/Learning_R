6faeae3314a336614f9c090ee06b736e44726383
omsouravsharma <souravsharma.sep22@gmail.com>
2022-12-24 22:14:36 +0530

    Importing data in R

--- GettingStarted.R
+++ GettingStarted.R
@@ -35,3 +35,24 @@ data("airmiles")
 
 require("graphics")
 plot(airmiles, main = "airmiles data", xlab = "Passanger data", col=4)
+
+# Entering data manually
+
+x1 = 1:100
+
+x2 = 10
+
+x3 <- seq(10)
+
+x4 <- seq(30,0, by = -3)
+
+x5 <- c(2,4,6,3,2,5,2) # Concatenate 
+
+x6 <- scan()
+
+
+# Importing data into R 
+data = read.table('C:/Users/NEXT/Desktop/Learning_R/Data/text.txt', header = TRUE, sep =",")
+str(data)
+
+rm(list = ls())
