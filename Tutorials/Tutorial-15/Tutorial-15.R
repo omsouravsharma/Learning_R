f5f8a3d2fe0642fbcbda4c06584f7e2dfee128fc
omsouravsharma <souravsharma.sep22@gmail.com>
2023-01-30 22:17:14 +0530

    Hitachi

--- Tutorials/Tutorial-15/Tutorial-15.R
+++ Tutorials/Tutorial-15/Tutorial-15.R
@@ -11,4 +11,22 @@ library(psych)
 # Fit attitude models
 
 attitude_fa <- fa(attitude, nfactors = 2, rotate = "oblimin", fm= "ml")
-attitude_fa
+attitude_fa # result
+
+# plot result 
+
+fa.diagram(attitude_fa, simple = FALSE)
+
+attitude_fa <- fa(attitude, nfactors = 2, rotate = "varimax", fm = "ml") 
+fa.diagram(attitude_fa, simple = FALSE)
+
+attitude_fa <- fa(attitude, nfactors = 2, rotate = "oblimin", fm = "miners") 
+fa.diagram(attitude_fa, simple = FALSE)
+
+scree(attitude, pc = FALSE)
+
+fa.parallel(attitude, fm = "ml", fa = "fa")
+
+
+# Latent class analysis
+
