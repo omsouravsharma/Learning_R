ed1d468a1d0de8070c8ccbb5e56e3d3a9181fa4f
omsouravsharma <souravsharma.sep22@gmail.com>
2023-03-07 22:36:07 +0530

    79 page

--- Finance_R/Chapter-6.R
+++ Finance_R/Chapter-6.R
@@ -28,3 +28,48 @@ INFY.PRC.RET$INFY.PRC.RET <- Delt(INFY.PRC.RET$Close)
 
 head(INFY.PRC.RET)
 
+#Logarithmic Total Return
+
+# formula rt = ln(pt/Pt-1) = ln(1+ Rt) = ln Pt = ln Pt-1 ln = natural logrithum
+
+# Calculate Log Returns
+
+INFY.LOG.RET <- INFY.PRC.RET$Close
+
+INFY.LOG.RET$INFY.LOG.RET <- diff(log(INFY.LOG.RET$Close))
+head(INFY.LOG.RET)
+
+# combine both return
+
+tot.rets<- cbind(INFY.PRC.RET[,2], INFY.LOG.RET[,2])
+head(tot.rets)
+
+max(abs(tot.rets$INFY.PRC.RET - tot.rets$INFY.LOG.RET), na.rm = TRUE)
+min(abs(tot.rets$INFY.PRC.RET - tot.rets$INFY.LOG.RET), na.rm = TRUE)
+
+# cumulative multi-day return
+INFY.ACM <- INFY.PRC.RET[,2]
+head(INFY.ACM)
+
+# Set first day to 0 
+INFY.ACM[1,1] <- 0
+head(INFY.ACM)
+
+# Calculate the Gross daily return
+
+INFY.ACM$GROSS.RET <- 1+INFY.ACM$INFY.PRC.RET
+head(INFY.ACM)
+
+#Calculate the cumulative Gross Return
+
+INFY.ACM$GROSS.CUM <- cumprod(INFY.ACM$GROSS.RET)
+head(INFY.ACM)
+
+#Convert Cumulative Return to Cumulative Net Return
+
+INFY.ACM$NET.CUM <- INFY.ACM$GROSS.CUM-1
+INFY.ACM[c(1:3, nrow(INFY.ACM)),]
+
+# Investment made at 2018 would return 157% in 2023
+
+
