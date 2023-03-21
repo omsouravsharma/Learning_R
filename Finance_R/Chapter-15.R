dd7704e2a86c73992efbad97be3347dc3d76f02f
omsouravsharma <souravsharma.sep22@gmail.com>
2023-03-21 22:18:12 +0530

    matrix

--- Finance_R/Chapter-15.R
+++ Finance_R/Chapter-15.R
@@ -1,1 +1,14 @@
 # Two Asset (Matrix Algebra)
+# Create Vector of Weights
+
+WGT.2asset <- c(0.25,0.75)
+WGT.2asset
+
+WGT.2asset <- matrix(WGT.2asset,1)
+WGT.2asset
+
+# Create a Transposed Vector of Weights
+tWGT.2asset<- t(WGT.2asset)
+tWGT.2asset
+
+#Construct Variance - Covariance Matrix
