a4f13f08270389eef751db6b848a336ba1c2d1ca
omsouravsharma <souravsharma.sep22@gmail.com>
2023-04-18 22:30:46 +0530

    RETS

--- Finance_R/Chapter-24- Factor.R
+++ Finance_R/Chapter-24- Factor.R
@@ -5,3 +5,40 @@
 # CAPM Regression 
 
 # Rolling Widow Regression
+
+# Libraries 
+library(xts)
+library(quantmod)
+library(ggplot2)
+library(plotly)
+
+
+result <- function(STOCK_TICK){
+  filename = paste("C:/Users/NEXT/Desktop/Learning_R/Data/stock/",STOCK_TICK,".NS.csv", sep = '')
+  data.STOCK_TICK = read.csv(filename, header = TRUE)
+  
+  data.STOCK_TICK$Date <-as.Date(data.STOCK_TICK$Date, format = "%Y-%m-%d")
+  data.STOCK_TICK <- data.STOCK_TICK[order(data.STOCK_TICK$Date), ]
+  data.STOCK_TICK <- as.xts(data.STOCK_TICK[, 2:7], order.by = data.STOCK_TICK$Date)
+  col_names <- ""
+  stock_col_name = strsplit(paste0(STOCK_TICK,".OPEN"," ", STOCK_TICK,".HIGH"," ", STOCK_TICK,".LOW"," ",STOCK_TICK,".CLOSE"," ",STOCK_TICK,".ADJ.CLOSE"," ",STOCK_TICK,".VOLUME"),split =" ")
+  for(i in stock_col_name){col_names<- c(i)}
+  names(data.STOCK_TICK) =col_names
+  return(data.STOCK_TICK)  
+}
+
+data.RELIANCE <- result("RELIANCE")
+data.NIFTY <- result("NSE")
+
+REL <- data.RELIANCE[,4]
+NIFTY <- data.NIFTY[,4]
+dim(REL)
+dim(NIFTY)
+rets <- diff(log(REL))
+rets$NIFTY<- diff(log(as.double(NIFTY)))
+
+class(NIFTY)
+b<- as.numeric(NIFTY)
+dim(b)
+names(rets)[1] <- "REL"
+
