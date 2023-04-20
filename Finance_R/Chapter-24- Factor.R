062f5ec37614f26f4bc42e800acb904faf0edd46
omsouravsharma <souravsharma.sep22@gmail.com>
2023-04-20 21:07:06 +0530

    event

--- Finance_R/Chapter-24- Factor.R
+++ Finance_R/Chapter-24- Factor.R
@@ -51,20 +51,53 @@ rets <- rets[-1,]
 rets[c(1:3, nrow(rets)),]
 
 #Create a rolling Window Regression Functions
-coeffs <- rollapply(rets, width = 251, FUN = function(X){
+coeffs <- rollapply(rets, width = 200, FUN = function(X){
   roll.reg = lm(REL~NIFTY, data = as.data.frame(X))
   return(roll.reg$coef)
 },
 by.column = FALSE)
 
-coeffs<-rollapply(rets,
-                     width=252,
-                    FUN=function(X)
-                       {
-                         roll.reg=lm(REL~NIFTY,
-                                       data=as.data.frame(X))
-                         return(roll.reg$coef)
-                         },
-                     by.column=FALSE)
+coeffs[c(1,200:201, nrow(coeffs)),]
 
-#Learn about rollapply function
+#Remove the NAs
+coeffs <- na.omit(coeffs)
+coeffs[c(1,3, nrow(coeffs)),]
+
+names(coeffs)<-c("Alpha","Beta")
+options(digits=3)
+coeffs[c(1:3,nrow(coeffs)),]
+
+#plot
+
+par(oma=c(0,0,4,0))
+par(mfrow=c(2,1))
+plot(x=index(coeffs),
+        xlab="Date",
+        y=coeffs$Alpha,
+        ylab="alpha",
+        type="l")
+plot(x=index(coeffs),
+        xlab="Date",
+        y=coeffs$Beta,
+        ylab="beta",
+        type="l")
+title(main="REL Alpha and Beta
+ Using Rolling 200-Day Windowns and",outer=TRUE)
+par(mfrow=c(1,1))
+
+
+multi.df <- cbind(data.frame(index(multi), data.frame(multi)))
+
+coeffs_df <- cbind(data.frame(index(coeffs), data.frame(coeffs)))
+
+plot_ly(c,  x = c$Datetype = 'scatter', mode= 'lines')
+fig <- plot_ly(multi.df, x = multi.df$Date, y=multi.df$SCHNEIDER.idx,name = "SCHNEIDER", type = 'scatter', mode= 'lines')
+
+fig <- plot_ly(coeffs_df, x = coeffs_df$index.coeffs, y = coeffs_df$Alpha, type = "scatter", mode= 'lines' , name = 'REL')
+#fig <- fig %>% add_trace(y =coeffs_df$Beta, name= "NIFTY")
+fig
+
+
+fig1 <- plot_ly(coeffs_df, x = coeffs_df$index.coeffs, y = coeffs_df$Beta, type = "scatter", mode= 'lines' , name = 'REL')
+#fig <- fig %>% add_trace(y =coeffs_df$Beta, name= "NIFTY")
+fig1
