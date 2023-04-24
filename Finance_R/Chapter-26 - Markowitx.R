fde1a818c3af1681969f5f81449d2e6b2570e379
omsouravsharma <souravsharma.sep22@gmail.com>
2023-04-24 22:28:32 +0530

    stock return

--- Finance_R/Chapter-26 - Markowitx.R
+++ Finance_R/Chapter-26 - Markowitx.R
@@ -1,1 +1,37 @@
-# Markowitz - Mean Variance Optimization. 
+# Stock Return calculator 
+
+# Libraries 
+library(xts)
+library(quantmod)
+library(ggplot2)
+library(plotly)
+
+result <- function(STOCK_TICK){
+  filename = paste("C:/Users/NEXT/Desktop/Learning_R/Data/stock/",STOCK_TICK,".NS.csv", sep = '')
+  data.STOCK_TICK = read.csv(filename, header = TRUE)
+  
+  data.STOCK_TICK$Date <-as.Date(data.STOCK_TICK$Date, format = "%Y-%m-%d")
+  data.STOCK_TICK <- data.STOCK_TICK[order(data.STOCK_TICK$Date), ]
+  data.STOCK_TICK <- as.xts(data.STOCK_TICK[, 2:7], order.by = data.STOCK_TICK$Date)
+  col_names <- ""
+  stock_col_name = strsplit(paste0(STOCK_TICK,".OPEN"," ", STOCK_TICK,".HIGH"," ", STOCK_TICK,".LOW"," ",STOCK_TICK,".CLOSE"," ",STOCK_TICK,".ADJ.CLOSE"," ",STOCK_TICK,".VOLUME"),split =" ")
+  for(i in stock_col_name){col_names<- c(i)}
+  names(data.STOCK_TICK) =col_names
+  return(data.STOCK_TICK)  
+}
+
+data.RELIANCE <- result("RELIANCE")
+
+str(data.RELIANCE)
+
+df <- cbind(data.frame(index(data.RELIANCE), data.frame(data.RELIANCE)))
+
+fig <- plot_ly(data = df, 
+               x = df$index.data.RELIANCE., 
+               y = df$RELIANCE.ADJ.CLOSE,
+               type = 'scatter',
+               mode = 'lines')
+fig
+
+# Calculate simple return
+https://rstudio-pubs-static.s3.amazonaws.com/78839_afca73ae18194eaf8f1b86d399dde969.html
